feaf704d4c25395a14817acc9b42d44637cd513d
shubhamsoni1979-alt <shubhamsoni1979@gmail.com>
2026-06-15 15:16:35 +0530

    chnaegs to color

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     '@nuxt/image'
   ],
-
+colorMode: {
+    preference: 'light',
+    fallback: 'light',
+  },
   css: ['~/assets/css/main.css']
 })
