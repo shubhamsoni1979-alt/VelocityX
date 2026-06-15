38f8ddb27521e43d5f6a0243fec2a6750575ec39
shubhamsoni1979-alt <shubhamsoni1979@gmail.com>
2026-06-15 14:53:22 +0530

    name change

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 import AppFoooter from '~/components/App-Foooter.vue';
-import AppHeader from '~/components/App-Header.vue';
+import AppHeader from '~/components/App-header.vue';
 
 </script>
 
-  <template>
-    <div class="min-h-screen flex flex-col">
-      <AppHeader/>
-      <main class="flex-1">
-        <slot />
-      </main>
-      <AppFoooter/>
-    </div>
-  </template>
+<template>
+  <div class="min-h-screen flex flex-col">
+    <AppHeader />
+    <main class="flex-1">
+      <slot />
+    </main>
+    <AppFoooter />
+  </div>
+</template>
